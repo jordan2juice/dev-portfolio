a31eff866c474830f868a29b92cb67c82a924d0b
jordan toujouse <jtoujo1@gmail.com>
2024-05-19 20:39:54 -0500

    updated my file

---
+++ src/app/projects/page.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+
+export default function Page() {
+  return (
+    <main>
+      <div>
+        <h1>Projects</h1>
+        <h2>News & Social Web Application</h2>
+        <p>
+          This site a web application is CRUD appilication that gets info from
+          FireStore and uses Firebase for Authentication.{" "}
+        </p>
+        <div>
+          <h3>Technologies used:</h3>
+          <p>React.js, Firebase, Firestore</p>
+        </div>
+        <div>
+          <h3>GitHub:</h3>
+          <p>
+            <a href="https://github.com/jordan2juice/info-page">
+              https://github.com/jordan2juice/info-page
+            </a>
+          </p>
+        </div>
+        <div>
+          <h3>Live Demo:</h3>
+          <p>
+            <a href="https://jordan2juice.github.io/info-page/">
+              https://jordan2juice.github.io/info-page/
+            </a>
+          </p>
+        </div>
+        <div>
+          <h3>Live Demo:</h3>
+          <p>
+            <a href="https://jordan2juice.github.io/info-page/">
+              https://jordan2juice.github.io/info-page/
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
